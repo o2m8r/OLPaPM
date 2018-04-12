a5b909e3212031a328cc1f594409d37779d1880f
theykneel <danchrnez@gmail.com>
2018-04-12 20:17:49 +0800

    Added php script & javascript for retrieving data from database

--- xcell.php
+++ xcell.php
@@ -9,7 +9,7 @@
 	    
     </head>
     
-<body onload="loadTable(<?php $results = mysqli_query($dbconnect,'SELECT MAX(xcell_tbl.RowCount) AS rows,MAX(xcell_tbl.ColCount) AS cols FROM xcell_tbl INNER JOIN users_tbl ON xcell_tbl.UserID = users_tbl.UserID INNER JOIN xcell_data_tbl ON xcell_tbl.XCellDataID = xcell_data_tbl.XCellDataID WHERE users_tbl.UserID = \'1\';');    while($row = $results->fetch_assoc()){ echo $row['rows'].','.$row['cols'];}?>);">
+<body onload="loadTable(<?php $results = mysqli_query($dbconnect,'SELECT MAX(xcell_tbl.RowCount) AS rows,MAX(xcell_tbl.ColCount) AS cols FROM xcell_tbl INNER JOIN users_tbl ON xcell_tbl.UserID = users_tbl.UserID INNER JOIN xcell_data_tbl ON xcell_tbl.XCellDataID = xcell_data_tbl.XCellDataID WHERE users_tbl.UserID = \'1\';');    while($row = $results->fetch_assoc()){ echo $row['rows'].','.$row['cols'];}?>);loadData();">
 
 
 <input type="button" onclick="make('addRow');" value="+ row">
@@ -34,13 +34,13 @@
     <td id="1A">
         <form method="post">
             <input type="hidden" name="coordinates" value="1,1">
-            <input type="text" name="field" onfocusout="if(this.value != '') { this.form.submit(); }">
+            <input type="text" name="field" id="field_1,1" onfocusout="if(this.value != '') { this.form.submit(); }">
         </form>
     </td>
     <td id="1B">
         <form method="post">
             <input type="hidden" name="coordinates" value="1,2">
-            <input type="text" name="field" onfocusout="if(this.value != '') { this.form.submit(); }">
+            <input type="text" name="field" id="field_1,2" onfocusout="if(this.value != '') { this.form.submit(); }">
         </form>
     </td>
   </tr>
@@ -48,12 +48,23 @@
  </tbody>
 </table>
 
-
-
-<br>
+<script>
+function loadData(){
+    <?php
+    
+        $results = mysqli_query($dbconnect,'SELECT xcell_data_tbl.Xrows AS row, xcell_data_tbl.Ycolumns AS col, xcell_data_tbl.FieldValue AS val FROM xcell_tbl INNER JOIN users_tbl ON xcell_tbl.UserID = users_tbl.UserID INNER JOIN xcell_data_tbl ON xcell_tbl.XCellDataID = xcell_data_tbl.XCellDataID WHERE users_tbl.UserID = \'1\';');
+        while($rows = $results->fetch_assoc()){ 
+            echo "document.getElementById('field_".$rows['row'].','.$rows['col']."').value = '".$rows['val']."';";
+        }
+        
+    ?>
+}
+</script>
 
 </body>
 </html>
+
+
 
 
 
